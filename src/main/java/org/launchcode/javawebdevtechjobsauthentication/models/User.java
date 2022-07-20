d13a6fd556019f3aeaaac33ed0f4c5d98dc6dbe9
Andie Johnson <ajohnson2@naic.org>
2022-07-20 09:26:00 -0500

    create and complete new User class

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/User.java
@@ -0,0 +1,36 @@
+package org.launchcode.javawebdevtechjobsauthentication.models;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class User extends AbstractEntity {
+
+	@NotNull
+	@NotBlank
+	@Size(min = 3, max = 20, message="username must be between 3 and 20 characters")
+	private String username;
+
+	@NotNull
+	@NotBlank
+	@Size(min = 5, max = 35, message="password must be between 5 and 35 characters")
+	private String passwordHash;
+
+	private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+	public User(String username, String password) {
+		this.username = username;
+		this.passwordHash = encoder.encode(password);
+	}
+
+	public User() {}
+
+	public String getUsername() {
+		return username;
+	}
+
+}
