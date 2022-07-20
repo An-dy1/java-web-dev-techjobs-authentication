a4c644888ae228ddbab6c1628505980f95ed6133
Andie Johnson <ajohnson2@naic.org>
2022-07-20 13:54:40 -0500

    login template and controllers done but not tested yet

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
@@ -6,9 +6,14 @@ import org.launchcode.javawebdevtechjobsauthentication.models.data.UserRepositor
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
@@ -23,12 +28,6 @@ public class AuthenticationController {
 
 	// method to set user's id value on the session's "user" key
 	public static void setUserInSession(HttpSession session, User user) {
-
-		// perhaps unnecessary
-//		if(session.getAttribute("user") != null) {
-//			return;
-//		}
-
 		session.setAttribute(userSessionKey, user.getId());
 
 	}
@@ -59,6 +58,45 @@ public class AuthenticationController {
 		model.addAttribute("loginDTO", new LoginFormDTO());
 
 		return "/login";
+
+	}
+
+	@PostMapping("/login")
+	public String processLoginRequest(@Valid @ModelAttribute LoginFormDTO loginDTO, Errors errors, Model model, HttpServletRequest request) {
+
+		// if the passed in values are not valid, return them to the login form
+		if(errors.hasErrors()) {
+			model.addAttribute("title", "login");
+			return "login";
+		}
+
+		// if no errors, then get user from database
+		User user = userRepository.findByUsername(loginDTO.getUsername());
+
+		// user could still be null, somehow
+		if(user == null) {
+			errors.rejectValue("username", "username.doesnotexist", "Sorry, that user is not in our system");
+			model.addAttribute("title", "Login");
+			return "login";
+		}
+
+		// or password might not match the one stored in the database
+		if(!user.isMatchingPassword(loginDTO.getPassword())) {
+			errors.rejectValue("password", "password.isnotcorrect", "Sorry, that password is not correct");
+			model.addAttribute("title", "Login");
+			return "login";
+		}
+
+		// finally, if there are no errors, the user exists, and the stored password matches the one entered
+		// then set the user in the session
+
+		setUserInSession(request.getSession(), user);
+
+		return "redirect:";
+	}
+
+	@GetMapping("/register")
+	public String getRegistrationForm(Model model) {
 
 	}
 }
